85f9de2e008909c7ea2aad91eb3ce862797932b2
Daria <dariaburdyga@gmail.com>
2019-08-23 11:27:59 +0300

    styles

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-
-      <div></div>
-
+      <div>
+          <div class="post" v-for="post in posts" :key="post.id">
+              <h3>Title: {{ post.title }}</h3>
+              <p>Description: {{ post.body }}</p>
+          </div>
+      </div>
   </div>
 </template>
 
 <script>
-    // import axios from 'axios'
+    import axios from 'axios'
 
     export default {
         name: 'HelloWorld',
@@ -20,24 +23,31 @@
                 posts: []
             }
         },
-
+        mounted() {
+            var self = this;
+            axios.get('https://jsonplaceholder.typicode.com/posts')
+                .then (function (res) {
+                    self.posts = res.data;
+                    console.log('Data: ', res.data);
+                })
+                .catch (function (error) {
+                    console.log('Error: ', error);
+                })
+        }
     }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    .post {
+        background: grey;
+        margin-bottom:  20px;
+        padding: 10px 20px;
+    }
+    h3 {
+        text-align: left;
+    }
+    p {
+        text-align: left;
+    }
 </style>
